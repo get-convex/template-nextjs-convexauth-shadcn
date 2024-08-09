19779bd08c68da193f35ddb2ad8e883e45a3c701
Michal Srb <srb@convex.dev>
2024-08-09 02:21:29 +0200

    Run Convex Auth setup command
    // This should be:
    // npx @convex-dev/auth
    // But we need a way to skip the env vars setup

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
 import type * as messages from "../messages.js";
 
 /**
@@ -24,6 +26,8 @@ import type * as messages from "../messages.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  http: typeof http;
   messages: typeof messages;
 }>;
 export declare const api: FilterApi<
