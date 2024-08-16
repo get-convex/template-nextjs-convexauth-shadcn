9af1a75658175f83db7b45d5d84e2b97a3adb5c1
Michal Srb <srb@convex.dev>
2024-08-15 17:03:36 -0700

    Add Convex Auth Nextjs providers and middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,13 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // `suppressHydrationWarning` only affects the html tag,
-    // and is needed by `ThemeProvider` which sets the theme
-    // class attribute on it
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
-      </body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      {/* `suppressHydrationWarning` only affects the html tag,
+      and is needed by `ThemeProvider` which sets the theme
+      class attribute on it */}
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class">{children}</ThemeProvider>
+        </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
   );
 }
